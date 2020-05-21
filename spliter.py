198c5fe4e2ab0bedaa829dc74092bbc45f40704c
Masanori HIRANO <masa.hirano.1996@gmail.com>
2020-05-22 03:07:32 +0900

    fix bug

--- spliter.py
+++ spliter.py
@@ -9,11 +9,8 @@ def split(day: str):
     df: pd.read_csv = pd.read_csv("data/StandardEquities_{}_out.csv".format(day))
     zip_file: zipfile.ZipFile = zipfile.ZipFile("save/{}.zip".format(day), mode="w")
 
-    def zip_save(i_ticker, group_df: pd.DataFrame, zip: zipfile.ZipFile):
+    for i_ticker, group in df.groupby("銘柄コード"):
         group_df.to_csv(zip.write("{}.csv".format(i_ticker)))
-
-    for i_ticker, group in df.groupby("銘柄コード")]):
-        zip_save(i_ticker=i_ticker, group_df=group, zip=zip_file)
 
 
 if __name__ == '__main__':
