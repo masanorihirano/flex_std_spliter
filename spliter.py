60bede40591693a8000e927229ee5f479a3d2437
Masanori HIRANO <masa.hirano.1996@gmail.com>
2020-05-22 01:37:31 +0900

    bug fix and added

--- spliter.py
+++ spliter.py
@@ -1,3 +1,4 @@
+import argparse
 import zipfile
 from joblib import Parallel, delayed
 import pandas as pd
@@ -8,8 +9,13 @@ def split(day: str):
     zip_file: zipfile.ZipFile = zipfile.ZipFile("save/{}.zip".format(day), mode="w")
 
     def zip_save(i_ticker, group_df: pd.DataFrame, zip: zipfile.ZipFile):
-        group_df.to_csv(zip_file.write("{}.csv".format(i_ticker)))
-    Parallel(n_jobs=-1, verbose=10)([delayed(zip_file)(i_ticker=i_ticker, group_df=group, zip=zip_file)
+        group_df.to_csv(zip.write("{}.csv".format(i_ticker)))
+    Parallel(n_jobs=-1, verbose=10)([delayed(zip_save)(i_ticker=i_ticker, group_df=group, zip=zip_file)
                                      for i_ticker, group in df.groupby("銘柄コード")])
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('day', help='processing day')
+    args = parser.parse_args()
+    split(day=args.day)
